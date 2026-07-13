ac83d9a8fb2e21fc008938af42d15a5e5f148756
zahidmohammeddot <zahidmohammed23088@gmail.com>
2026-07-13 08:26:47 +0530

    First Push

---
+++ script.js
@@ -0,0 +1,76 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// ================= topic-search-section =================
+
+function searchTopics() {
+
+    let input = document.getElementById("topicSearch");
+    let filter = input.value.toLowerCase();
+
+    let cards = document.getElementsByClassName("topic-card");
+
+    for (let i = 0; i < cards.length; i++) {
+
+        let text = cards[i].textContent.toLowerCase();
+
+        if (text.includes(filter)) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+// ================= video-search-section =================
+
+
+// function searchVideos() {
+
+//     let input = document.getElementById("topicSearch");
+//     let filter = input.value.toLowerCase();
+
+//     let videos = document.getElementsByClassName("subject-video-card");
+
+//     for (let i = 0; i < videos.length; i++) {
+
+//         let title = videos[i].textContent.toLowerCase();
+
+//         if (title.includes(filter)) {
+//             videos[i].style.display = "";
+//         } else {
+//             videos[i].style.display = "none";
+//         }
+//     }
+// }
+
+
+
+// Back To Top Button
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 300){
+        backToTop.style.display = "block";
+    }
+    else{
+        backToTop.style.display = "none";
+    }
+
+});
+
+backToTop.addEventListener("click", function(){
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
+
